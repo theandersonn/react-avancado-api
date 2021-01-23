47a8ab859728cc5897f2eb0cb780c59fa0d5c032
theandersonn <theandersonn@gmail.com>
2021-01-23 13:57:41 -0300

    netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const axios = require("axios");
+const netlifyWebhook = strapi.config.get("custom.netlifyWebhook");
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook && axios.post(netlifyWebhook);
+    },
+    async beforeCreate(_, __, ___) {
+      netlifyWebhook && axios.post(netlifyWebhook);
+    },
+  },
+};
